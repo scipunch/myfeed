d0403287a6b17cb76429a8149a01bc967c071651
scipunch <scipunch@gmail.com>
2025-07-23 18:09:49 +0200

    chore: Move all relate to config logic into one package

--- config/config.go
+++ config/config.go
@@ -1,9 +1,15 @@
 package config
 
 import (
+	"fmt"
+	"log/slog"
 	"os"
 	"path"
+
+	"github.com/BurntSushi/toml"
 )
+
+const baseCfgPath = "myfeed/config.toml"
 
 type Config struct {
 	Resources    []ResourceConfig `toml:"resources"`
@@ -23,9 +29,39 @@ var (
 	TorrentParserT  = ParserT("torrent")
 )
 
+func Read(path string) (Config, error) {
+	conf := Default()
+	if path != DefaultPath() {
+		dat, err := os.ReadFile(path)
+		if err != nil {
+			return conf, fmt.Errorf("failed to find config file at %s with %w", path, err)
+		}
+		_, err = toml.Decode(string(dat), &conf)
+		if err != nil {
+			return conf, fmt.Errorf("failed to decode config at %s with %w", path, err)
+		}
+		slog.Debug("initialization finished", "configured resources", len(conf.Resources))
+	}
+	return conf, nil
+}
+
 func Default() Config {
 	var dbBase = path.Join(os.Getenv("HOME"), ".local/share/myfeed")
 	return Config{
 		DatabasePath: path.Join(dbBase, "data.db"),
 	}
+}
+
+func DefaultPath() string {
+	var xdgHome = os.Getenv("XDG_CONFIG_HOME")
+	if xdgHome != "" {
+		return path.Join(xdgHome, baseCfgPath)
+	}
+
+	var home = os.Getenv("HOME")
+	if home != "" {
+		return path.Join(xdgHome, ".config", baseCfgPath)
+	}
+
+	panic("unclear where to search for the config fie")
 }
--- main.go
+++ main.go
@@ -13,13 +13,10 @@ import (
 	"path"
 	"syscall"
 
-	"github.com/BurntSushi/toml"
 	_ "modernc.org/sqlite"
 
 	"github.com/scipunch/myfeed/config"
 )
-
-const baseCfgPath = "myfeed/config.toml"
 
 //go:embed schema.sql
 var ddl string
@@ -30,25 +27,16 @@ func main() {
 	}
 
 	var cfgPath string
-	flag.StringVar(&cfgPath, "config", defaultCfg(), "path to a TOML config")
+	flag.StringVar(&cfgPath, "config", config.DefaultPath(), "path to a TOML config")
 	flag.Parse()
 
-	conf := config.Default()
-	if cfgPath != defaultCfg() {
-		dat, err := os.ReadFile(cfgPath)
-		if err != nil {
-			log.Fatalf("failed to find config file at %s", cfgPath)
-		}
-
-		_, err = toml.Decode(string(dat), &conf)
-		if err != nil {
-			log.Fatalf("failed to decode config at %s", cfgPath)
-		}
-		slog.Debug("initialization finished", "configured resources", len(conf.Resources))
+	conf, err := config.Read(cfgPath)
+	if err != nil {
+		log.Fatalf("failed to read config with %s", err)
 	}
 
 	dbBasePath := path.Dir(conf.DatabasePath)
-	err := os.MkdirAll(dbBasePath, os.ModePerm)
+	err = os.MkdirAll(dbBasePath, os.ModePerm)
 	if err != nil {
 		log.Fatalf("failed to create base shared directory at '%s' with %s", dbBasePath, err)
 	}
@@ -60,20 +48,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to initialize database schema with %v", err)
 	}
-}
-
-func defaultCfg() string {
-	var xdgHome = os.Getenv("XDG_CONFIG_HOME")
-	if xdgHome != "" {
-		return path.Join(xdgHome, baseCfgPath)
-	}
-
-	var home = os.Getenv("HOME")
-	if home != "" {
-		return path.Join(xdgHome, ".config", baseCfgPath)
-	}
-
-	panic("unclear where to search for the config fie")
 }
 
 func initDB(ctx context.Context, source string) (*sql.DB, error) {
